e559a36cc01f9b027e9196cba68bcc950e18ce29
DeadShot <deadshot@kali.Linux>
2021-02-07 15:46:25 +0500

    Update for settings.py static_url

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(VENV_PATH, 'MyBlog/media_root')
